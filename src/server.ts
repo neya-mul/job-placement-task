27bf799ba27517e1dc6ffad5e2d641ed6c51ede5
neya-mul <neyamulislam946@gmail.com>
2026-08-10 19:18:06 +0600

    first api created

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { prisma } from "./lib/prisma";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/users", async (req, res) => {
+  const user = await prisma.user.create({
+    data: req.body,
+  });
+
+  res.json({
+    success: true,
+    data: user,
+  });
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+
+  res.json({
+    success: true,
+    data: users,
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
